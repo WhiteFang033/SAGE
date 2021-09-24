abe281fc0e6249bdc9a9de890ac8b5ed5f33924a
WhiteFang <anjugyanprakash@gmail.com>
2021-09-24 16:01:27 +0530

    opus fixed finger's crossed

--- source/boot.py
+++ source/boot.py
@@ -5,9 +5,6 @@ from discord_components import *
 intents = discord.Intents.all()
 sage = commands.Bot(command_prefix="!", intents= intents)
 
-if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
-
 @sage.event
 async def on_ready():
     await sage.change_presence(status= discord.Status.idle, activity= discord.Game('Shinra Tensei'))
